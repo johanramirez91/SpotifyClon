0d5bb2043a2bcea4010e3ed289bc3f7ea286f135
Johan Ramirez <johan.ramirez@sofka.com.co>
2022-02-21 17:09:48 -0500

    Scafold components and Example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ExampleComponent } from './example/example.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', //TODO: localhost
+    component: ExampleComponent 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spotify-app';
-
-  status: object | null = {}
 }
